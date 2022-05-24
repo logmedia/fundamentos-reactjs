0d3bf0138b9887ed8e3c2a839250ea44c741131e
logmedia <estudioz@gmail.com>
2022-05-24 12:14:36 -0300

    Atualizaçao React 18

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,18 @@
+interface RepositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  }
+}
+
+export function RepositoryItem(props: RepositoryItemProps){
+  const { name, description , html_url } = props.repository   
+  return(
+    <li>
+      <strong>{name}</strong>
+      <p>{description}</p>
+      <a href={html_url}>Acessar repositório refresh</a> 
+    </li>
+  )
+}
